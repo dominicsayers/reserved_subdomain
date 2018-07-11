9ab594ecdc52f0fb3a06c362a07764866ff1dfed
Dominic Sayers <dominic@sayers.cc>
2018-07-11 11:03:06 +0100

    Subdomains reserved by pattern

---
+++ lib/reserved_subdomain/pattern.rb
@@ -0,0 +1,20 @@
+class ReservedSubdomain
+  class Pattern
+    PATTERNS = [
+      /.*[_-]feed$/,
+      /.*[_-]support$/,
+      /^v[0-9]{,2}$/,
+      /^xn--/,
+    ].freeze
+
+    attr_reader :subdomain
+
+    def initialize(subdomain)
+      @subdomain = subdomain
+    end
+
+    def reserved?
+      PATTERNS.any? { |pattern| subdomain =~ pattern }
+    end
+  end
+end
